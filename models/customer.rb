b56fa81bca96a5778c55adc1c550a17e3df151ea
annalanigan <annalanigan@outlook.com>
2017-11-17 11:17:36 +0000

    starting to build the classes

---
+++ models/customer.rb
@@ -0,0 +1,16 @@
+require('../db/sql_runner')
+
+class customer
+
+  attr_reader :id, :name
+  attr_accessor :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+
+
+end
---
+++ models/film.rb
@@ -0,0 +1,13 @@
+require('../db/sql_runner')
+
+class Film
+
+  attr_reader :id, :title, :price
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @price = options['price']
+  end
+
+end
